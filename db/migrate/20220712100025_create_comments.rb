3230783b8e99d1d88b5cbe3ee0cce77df3070c01
moeisaji <moe.isaji@gmail.com>
2022-07-28 10:19:34 +0000

    マイグレーションファイル修正

--- db/migrate/20220712100025_create_comments.rb
+++ db/migrate/20220712100025_create_comments.rb
@@ -3,9 +3,9 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
 
       t.timestamps
-      t.integer :article_id, null: false, default: ""
-      t.integer :customer_id, null: false, default: ""
-      t.text :comment, null: false, default: ""
+      t.integer :article_id, null: false
+      t.integer :customer_id, null: false
+      t.text :comment, null: false
     end
   end
 end
